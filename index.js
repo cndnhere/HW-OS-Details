c8ae36176cec90201b7d5a08650f47c9c75ff677
cndnhere <98242648+cndnhere@users.noreply.github.com>
2023-12-08 18:03:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,83 @@
+// server.js
 const express = require('express');
-const os = require('os');
+const cors = require('cors');
+const si = require('systeminformation');
+const { exec } = require('child_process');
 
 const app = express();
-const port = 3000;
+app.use(cors());
 
-app.get('/os-details', (req, res) => {
-  const osDetails = {
-    platform: os.platform(),
-    type: os.type(),
-    release: os.release(),
-    architecture: os.arch(),
-    hostname: os.hostname(),
-  };
+app.get('/cpu-serial-number', async (req, res) => {
+    getHardDriveSerialNumber()
+    .then(serialNumbers => {
+      console.log('Hard Drive Serial Numbers:', serialNumbers);
+      hwSrNo = serialNumbers;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  
+    getCPUSerialNumber()
+    .then(serialNumber => {
+      console.log('CPU Serial Number:', serialNumber);
+      cpuSrNo = serialNumber
 
-  res.json(osDetails);
+      res.json(serialNumber)
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  
+
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+
+
+function getCPUSerialNumber() {
+    return new Promise((resolve, reject) => {
+      exec('wmic cpu get ProcessorId', (error, stdout, stderr) => {
+        if (error) {
+          reject(`Error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          reject(`Command execution error: ${stderr}`);
+          return;
+        }
+  
+        const serialNumber = stdout.split('\n')[1].trim();
+        resolve(serialNumber);
+      });
+    });
+  }
+  
+  
+  
+  
+  
+  
+  function getHardDriveSerialNumber() {
+    return new Promise((resolve, reject) => {
+      exec('wmic diskdrive get SerialNumber', (error, stdout, stderr) => {
+        if (error) {
+          reject(`Error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          reject(`Command execution error: ${stderr}`);
+          return;
+        }
+  
+        const serialNumbers = stdout.split('\n').slice(1).map(line => line.trim()).filter(Boolean);
+        resolve(serialNumbers);
+      });
+    });
+  }
+  
+ 
+  
+
+const PORT = 3000; // Change to your desired port
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
